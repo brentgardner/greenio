2482a676f96969ad5ab7a0a354581e201f80b3c4
Brent Gardner <btgardner@gmail.com>
2015-07-09 19:44:24 -0500

    updated model

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var ip_addr = '127.0.0.1';
 var port = '8080';
 var connection_string = '127.0.0.1:27017/greenio';
 var db = mongojs(connection_string, ['greenio']);
-var jobs = db.collection("readings");
+var readings = db.collection("readings");
 
 var server = restify.createServer({
   name : 'greenio'
@@ -25,9 +25,22 @@ server.listen(port, ip_addr, function(){
   console.log('%s listening at %s ', server.name, server.url);
 });
 
+/**
+ * @api {get} /readings Request All Readings
+ * @apiName findAllReadings
+ * @apiGroup Readings
+ *
+ * @apiSuccess {Number} deviceId The numeric Id of the device transmitting the reading.
+ * @apiSuccess {Date} readOn The Date and Time that the reading was made.
+ * @apiSuccess {Number} ph The PH of the liquid mixture.
+ * @apiSuccess {Number} airTemp The air Tempurature.
+ * @apiSuccess {Number} liqTemp The liauid Tempurature.
+ * @apiSuccess {Object} location The GPS cords of the device.
+ * @apiSuccess {Date} postedOn The Date and Time that the reading was written.
+ */
 function findAllReadings(req, res , next){
     res.setHeader('Access-Control-Allow-Origin','*');
-    jobs.find().limit(20).sort({postedOn : -1} , function(err , success){
+    readings.find().limit(20).sort({postedOn : -1} , function(err , success){
         console.log('Response success '+success);
         console.log('Response error '+err);
         if(success){
@@ -41,9 +54,24 @@ function findAllReadings(req, res , next){
 
 }
 
+/**
+ * @api {get} /readings/:id Request User information
+ * @apiName postNewReading
+ * @apiGroup Readings
+ *
+ * @apiParam {Number} _Id readings unique ID.
+ *
+ * @apiSuccess {Number} deviceId The numeric Id of the device transmitting the reading.
+ * @apiSuccess {Date} readOn The Date and Time that the reading was made.
+ * @apiSuccess {Number} ph The PH of the liquid mixture.
+ * @apiSuccess {Number} airTemp The air Tempurature.
+ * @apiSuccess {Number} liqTemp The liauid Tempurature.
+ * @apiSuccess {Object} location The GPS cords of the device.
+ * @apiSuccess {Date} postedOn The Date and Time that the reading was written.
+ */
 function findReading(req, res , next){
     res.setHeader('Access-Control-Allow-Origin','*');
-    jobs.findOne({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
+    readings.findOne({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
         console.log('Response success '+success);
         console.log('Response error '+err);
         if(success){
@@ -54,20 +82,39 @@ function findReading(req, res , next){
     })
 }
 
+/**
+ * @api {post} /readings
+ * @apiName postNewReading
+ * @apiGroup Readings
+ *
+ * @apiParam {Object} reading object.
+ *
+ * @apiSuccess {String} _id The unique ID of the reading.
+ * @apiSuccess {Number} deviceId The numeric Id of the device transmitting the reading.
+ * @apiSuccess {Date} readOn The Date and Time that the reading was made.
+ * @apiSuccess {Number} ph The PH of the liquid mixture.
+ * @apiSuccess {Number} airTemp The air Tempurature.
+ * @apiSuccess {Number} liqTemp The liauid Tempurature.
+ * @apiSuccess {Object} location The GPS cords of the device.
+ * @apiSuccess {Date} postedOn The Date and Time that the reading was written.
+ */
 function postNewReading(req , res , next){
-    var job = {};
-    job.title = req.params.title;
-    job.description = req.params.description;
-    job.location = req.params.location;
-    job.postedOn = new Date();
+    var Reading = {};
+    reading.deviceId = req.params.deviceId;
+    reading.readOn = req.params.readOn;
+    reading.ph = req.params.ph;
+    reading.airTemp = req.params.airTemp;
+    reading.liqTemp = req.params.liqTemp;
+    reading.location = req.params.location;
+    reading.postedOn = new Date();
 
     res.setHeader('Access-Control-Allow-Origin','*');
 
-    jobs.save(job , function(err , success){
+    readings.save(reading , function(err , success){
         console.log('Response success '+success);
         console.log('Response error '+err);
         if(success){
-            res.send(201 , job);
+            res.send(201 , reading);
             return next();
         }else{
             return next(err);
@@ -75,9 +122,16 @@ function postNewReading(req , res , next){
     });
 }
 
+/**
+ * @api {post} /readings
+ * @apiName postNewReading
+ * @apiGroup Readings
+ *
+ * @apiParam {String} readingId unique id of the reading.
+ */
 function deleteReading(req , res , next){
     res.setHeader('Access-Control-Allow-Origin','*');
-    jobs.remove({_id:mongojs.ObjectId(req.params.jobId)} , function(err , success){
+    readings.remove({_id:mongojs.ObjectId(req.params.readingId)} , function(err , success){
         console.log('Response success '+success);
         console.log('Response error '+err);
         if(success){
